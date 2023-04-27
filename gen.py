e668df82fb310cc5895872f883d98ba829a6d153
michelelaig <michele.leigheb@protonmail.com>
2023-04-27 15:04:15 +0200

    modified:   es.json 	modified:   gen.py 	done.csv 	show.py

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from random import choice
 import json
 #cap_es = {1 :33,2 :27, 3:43, 4:27,5 : 0}
@@ -10,17 +11,22 @@ with open("es.json","r") as f:
 
 tipo = choice(["esr","esr","esm"])
 
+t = str(datetime.date(datetime.today()))
 with open("done.csv","r") as f:
-	fatti = [s.strip() for s in f.readlines()]
-#print(fatti)
+	lst = f.readlines()
+	date = [s.strip() for s in lst if "2023" in s]
+	fatti = [s.strip() for s in lst if "2023" not in s]
+	fatti_oggi = [s.strip() for s in lst[lst.index(t+'\n')+1::]]
 
+cap_preferiti = ["4"]
 lista_possibilita = []
 while not lista_possibilita:
-	capitolo = choice(list(diz.keys()))
-	#print(capitolo)
-	lista_possibilita = [f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"])) if f"{capitolo} {tipo} {i}" not in fatti]*5
-	lista_possibilita.extend([f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"]))])
-	#print(lista_possibilita)
+	cs = list(diz.keys())+cap_preferiti*10
+	capitolo = choice(list(cs))
+	lista_possibilita = [f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"])) if f"{capitolo} {tipo} {i}" not in fatti]*9 #semplicemente i non fatti
+	lista_possibilita.extend([lista_possibilita[-1]]*9)
+	lista_possibilita.extend([f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"])) if f"{capitolo} {tipo} {i}" not in fatti_oggi]) #tutti quanti
+
 
 
 
@@ -30,13 +36,14 @@ while not lista_possibilita:
 #print(lista_possibilita)
 es = choice(lista_possibilita)
 print(f"{capitolo}: {es}")
-
-
-
 with open("done.csv","a") as f:
 	print(f"{capitolo} {es}",file=f)
+	if t  not in date:
+		print(t,file=f)
+
+
 n = int(es.split(" ")[1])
-if n==diz[capitolo][f"n_{tipo}"] and  n<diz[capitolo][f"n_{tipo}_max"] :
+if n>=diz[capitolo][f"n_{tipo}"] and  n<diz[capitolo][f"n_{tipo}_max"] :
 	print("Si progredisce verso l'infinito :)")
 	diz[capitolo][f"n_{tipo}"]+=1 
 
