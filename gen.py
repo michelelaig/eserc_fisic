a3408c5a302dc9c662e62bba70839f96e8b2e30e
michelelaig <michelelaig>
2023-05-04 19:18:48 +0200

    modified:   done.csv 	modified:   es.json 	modified:   gen.py

--- gen.py
+++ gen.py
@@ -14,20 +14,34 @@ tipo = choice(["esr","esr"])#,"esm"])
 t = str(datetime.date(datetime.now()))
 with open("done.csv","r") as f:
 	lst = f.readlines()
-	date = [s.strip() for s in lst if "2023" in s]
-	fatti = [s.strip() for s in lst if "2023" not in s]
+	date  = [s.strip() for s in lst if "2023" in s]
+		
+	dubbi = [s.strip(" dubbio\n") for s in lst if "dubbio" in s and not s.strip(" dubbio\n")+"\n" in lst]
+	fatti = [s.strip(" dubbio\n"). strip() for s in lst if "2023" not in s]
+	#print(dubbi)
+	#print(fatti)
+	fatti_diz = {f : fatti.count(f) for f in fatti}
+	m = max(fatti_diz.values())
+	fatti_oggi = []
 	if t+'\n' in lst:
 		fatti_oggi = [s.strip() for s in lst[lst.index(t+'\n')+1::]]
-	else:
-		fatti_oggi = []
 cap_preferiti = ["4","5"]
 lista_possibilita = []
+
+def occorrenze(s):
+	if s in fatti_oggi:
+		return m+1
+	if s in dubbi:
+		return 0
+	if s in fatti_diz:
+		return fatti_diz[s]
+	return 0
+
 while not lista_possibilita:
 	cs = list(diz.keys())+cap_preferiti*10
 	capitolo = choice(list(cs))
-	lista_possibilita = [f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"])) if f"{capitolo} {tipo} {i}" not in fatti]*9 #semplicemente i non fatti
+	lista_possibilita = [f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"]))for j in range(m-occorrenze(f"{capitolo} {tipo} {i}"))  ]
 	lista_possibilita.extend([lista_possibilita[-1]]*9)
-	lista_possibilita.extend([f"{tipo} {i}" for i in range(1,1+int(diz[capitolo][f"n_{tipo}"])) if f"{capitolo} {tipo} {i}" not in fatti_oggi]) #tutti quanti
 
 
 
@@ -39,7 +53,7 @@ media = (
 
 #print(lista_possibilita)
 es = choice(lista_possibilita)
-feedback = input(f"Oggi ne hai fatti {len(fatti_oggi)}.\nLa media è di {media} al giorno, ora facciamo il {es} del capitolo {capitolo}\n")
+feedback = input(f"Con questo ne fai {1+len(fatti_oggi)}.\nLa media è di {media} al giorno, ora facciamo il {es} del capitolo {capitolo}\n")
 if feedback=="no":
 	print("niente")
 else:
@@ -56,6 +70,5 @@ else:
 
 	with open("es.json","w") as f:
 		json.dump(diz,f)
-
 
 
